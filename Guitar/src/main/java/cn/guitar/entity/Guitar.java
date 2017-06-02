28592f2609a8045482bdf1bcfd6650cef0953f54
邓忍 <1105567502@qq.com>
2017-06-02 20:56:16 +0800

    update

--- Guitar/src/main/java/cn/guitar/entity/Guitar.java
+++ Guitar/src/main/java/cn/guitar/entity/Guitar.java
@@ -1,14 +1,5 @@
 package cn.guitar.entity;
 
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
-@Entity
-@Table(name="guitar",uniqueConstraints = { @UniqueConstraint(columnNames = "serialNumber")})
 public class Guitar implements java.io.Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -20,50 +11,42 @@ public class Guitar implements java.io.Serializable{
 	private String backWood; 
 	private String topWood;  
 	
-	@Id
-	@Column(name="serialNumber")
 	public String getSerialNumber() {
 		return serialNumber;
 	}
 	public void setSerialNumber(String serialNumber) {
 		this.serialNumber = serialNumber;
 	}
-	@Column(name="price")
 	public double getPrice() {
 		return price;
 	}
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	@Column(name="builder")
 	public String getBuilder() {
 		return builder;
 	}
 	public void setBuilder(String builder) {
 		this.builder = builder;
 	}
-	@Column(name="model")
 	public String getModel() {
 		return model;
 	}
 	public void setModel(String model) {
 		this.model = model;
 	}
-	@Column(name="type")
 	public String getType() {
 		return type;
 	}
 	public void setType(String type) {
 		this.type = type;
 	}
-	@Column(name="backWood")
 	public String getBackWood() {
 		return backWood;
 	}
 	public void setBackWood(String backWood) {
 		this.backWood = backWood;
 	}
-	@Column(name="topWood")
 	public String getTopWood() {
 		return topWood;
 	}
